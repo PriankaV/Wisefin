e1c833415e9bb693b27da5601e8f3a31c96d09c3
Prianka <priankav9@gmail.com>
2025-02-17 15:10:05 -0500

    Updated Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "WiseFin",
+  description: "Take control of your spending, set financial goals, and gain AI-driven insights into your transactions.",
   icons: {
     icon: '/icons/logo.svg'
   }
